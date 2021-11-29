6278beaf5d51b784abd14f42e296ccdf672fe01f
Magnus Dobner <dobner.magnus@stud.hs-fresenius.de>
2021-11-29 15:01:54 +0100

    Unity Fixx

---
+++ Assets/Scripts/Magnus Skirpts/Rotator.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public Quaternion startQuarternion;
+
+    public Vector3 quaternionToCevtor;
+    public Vector3 reverseQuaternion;
+
+    public float lerpTime = 1;
+    public float RotateAmound = 1;
+
+    public bool rotate;
+    public bool rotateConstantly;
+
+    void Start()
+    {
+        startQuarternion = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(rotate)
+        transform.rotation = Quaternion.Lerp(transform.rotation , Quaternion.Euler(reverseQuaternion) ,Time.deltaTime * lerpTime);
+      if(rotateConstantly && !rotate)
+        transform.Rotate(Vector3.forward * RotateAmound); //right //up /forward
+    }
+    public void snapRotation()
+    {
+        transform.rotation = startQuarternion;
+    }
+}
