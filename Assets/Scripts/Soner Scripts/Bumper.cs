1b24f377405b16a76ade9938fd4ae8274d27dc6b
Soner Kaplan <sonykaos27@googlemail.com>
2021-12-01 13:59:58 +0100

    Bumper Added

---
+++ Assets/Scripts/Soner Scripts/Bumper.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bumper : MonoBehaviour
+{
+
+    [SerializeField] float bounceAmount = 300;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Rigidbody playerRigidbody = other.gameObject.GetComponent<Rigidbody>();
+
+            if (playerRigidbody != null)
+            {
+                Debug.Log("Found player rigidbody");
+
+                Vector3 bounceDirection = -playerRigidbody.velocity;
+
+                playerRigidbody.AddForce(bounceDirection * bounceAmount);
+            }
+        }
+    }
+
+}
