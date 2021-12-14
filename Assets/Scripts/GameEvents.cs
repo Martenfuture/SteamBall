f03416cdaa945c504eada23a8d097f3702a44098
Marten <martenkahl@outlook.de>
2021-12-14 17:41:50 +0100

    added plate puzzle + prefab clean up

--- Assets/Scripts/GameEvents.cs
+++ Assets/Scripts/GameEvents.cs
@@ -6,12 +6,23 @@ using UnityEngine;
 public class GameEvents : MonoBehaviour
 {
     public static GameEvents puzzleButton;
+    public static GameEvents interactionControl;
     private void Awake()
     {
         puzzleButton = this;
+        interactionControl = this;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            InteractionEnter();
+        }
     }
 
     public event Action<int> onButtonTriggerEnter;
+    public event Action onInteractionEnter;
     public void ButtonTriggerEnter(int id)
     {
         if (onButtonTriggerEnter != null)
@@ -19,4 +30,13 @@ public class GameEvents : MonoBehaviour
             onButtonTriggerEnter(id);
         }
     }
+
+    public void InteractionEnter()
+    {
+        if (onButtonTriggerEnter != null)
+        {
+            onInteractionEnter();
+        }
+    }
+
 }
