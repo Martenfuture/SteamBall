948a8d2138a4fb7b451faa4739440aadb81826b3
iDepex <dominik.schilde@t-online.de>
2021-12-16 20:47:19 +0100

    CamUpdate

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -9,8 +9,7 @@ public class ThirdPersonMovement : MonoBehaviour
 
     public float speed = 6f;
 
-    public float turnSmoothTime = 0.1f;
-    float turnSmoothVelocity;
+    
 
 
     // Update is called once per frame
@@ -23,8 +22,8 @@ public class ThirdPersonMovement : MonoBehaviour
         if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+     
+            
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
