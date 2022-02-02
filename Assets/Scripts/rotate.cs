fcba35b5806139f615bde587742bf2e9b7212912
Marten <martenkahl@outlook.de>
2022-02-02 14:08:06 +0100

    fixed bee + fixed ring roiation

--- Assets/Scripts/rotate.cs
+++ Assets/Scripts/rotate.cs
@@ -6,14 +6,18 @@ public class rotate : MonoBehaviour
 {
     
     public float rotationSpeed;
+    public GameObject camera;
+    public float rotationOffset;
 
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        Vector3 movementDirection = new Vector3(verticalInput, 0, -horizontalInput);
+        Vector3 movementDirection = Quaternion.Euler(0, camera.transform.eulerAngles.y + rotationOffset, 0) * new Vector3(verticalInput, 0, -horizontalInput); // added roatation relevant to the camera
         movementDirection.Normalize();
+
+        
 
 
         if (movementDirection != Vector3.zero)
